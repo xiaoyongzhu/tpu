20a1adad3da1437525eaf88b7e8182be091326bd
Xiaoyong Zhu <xiaoyzhu@microsoft.com>
2018-05-12 20:43:28 -0700

    update batch size

--- models/official/retinanet/retinanet_main.py
+++ models/official/retinanet/retinanet_main.py
@@ -127,6 +127,7 @@ def main(argv):
       resnet_checkpoint=FLAGS.resnet_checkpoint,
       val_json_file=FLAGS.val_json_file,
       mode=FLAGS.mode,
+      batch_size=FLAGS.train_batch_size,
   )
   config_proto = tf.ConfigProto(
       allow_soft_placement=True, log_device_placement=False)
